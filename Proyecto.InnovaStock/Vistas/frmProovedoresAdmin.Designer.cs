5b34dbbf83759bee86dd764e0af36c9517556690
sisojoseph14-ui <sisojoseph14@gmail.com>
2025-12-07 02:12:13 -0500

    Se arreglo proveedor

--- Proyecto.InnovaStock/Vistas/frmProovedoresAdmin.Designer.cs
+++ Proyecto.InnovaStock/Vistas/frmProovedoresAdmin.Designer.cs
@@ -29,30 +29,38 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
-            dgvProveedores = new DataGridView();
-            label1 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            textBox1 = new TextBox();
-            textBox3 = new TextBox();
-            textBox5 = new TextBox();
-            comboBox1 = new ComboBox();
-            button1 = new Button();
             grpProveedor = new GroupBox();
+            lblEditar = new Label();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            textBox5 = new TextBox();
+            textBox3 = new TextBox();
+            textBox1 = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label1 = new Label();
+            dgvProveedores = new DataGridView();
+            panel1 = new Panel();
+            cbxEliminar = new CheckBox();
+            cbxEditar = new CheckBox();
+            cbxAñadir = new CheckBox();
             tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvProveedores).BeginInit();
             grpProveedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProveedores).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.BackColor = Color.Transparent;
             tableLayoutPanel1.BackgroundImage = Properties.Resources.fondo;
-            tableLayoutPanel1.ColumnCount = 1;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 1055F));
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 903F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 172F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
             tableLayoutPanel1.Controls.Add(grpProveedor, 0, 1);
+            tableLayoutPanel1.Controls.Add(panel1, 1, 0);
             tableLayoutPanel1.Controls.Add(dgvProveedores, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
@@ -64,26 +72,100 @@
             tableLayoutPanel1.TabIndex = 2;
             tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
             // 
-            // dgvProveedores
+            // grpProveedor
             // 
-            dgvProveedores.Anchor = AnchorStyles.None;
-            dgvProveedores.BackgroundColor = Color.White;
-            dgvProveedores.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvProveedores.Location = new Point(265, 7);
-            dgvProveedores.Name = "dgvProveedores";
-            dgvProveedores.Size = new Size(536, 158);
-            dgvProveedores.TabIndex = 3;
+            grpProveedor.Anchor = AnchorStyles.Right;
+            grpProveedor.BackgroundImage = Properties.Resources.fondo;
+            grpProveedor.Controls.Add(lblEditar);
+            grpProveedor.Controls.Add(button1);
+            grpProveedor.Controls.Add(comboBox1);
+            grpProveedor.Controls.Add(textBox5);
+            grpProveedor.Controls.Add(textBox3);
+            grpProveedor.Controls.Add(textBox1);
+            grpProveedor.Controls.Add(label6);
+            grpProveedor.Controls.Add(label5);
+            grpProveedor.Controls.Add(label4);
+            grpProveedor.Controls.Add(label1);
+            grpProveedor.Font = new Font("Franklin Gothic Heavy", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            grpProveedor.ForeColor = Color.White;
+            grpProveedor.Location = new Point(223, 176);
+            grpProveedor.Name = "grpProveedor";
+            grpProveedor.Size = new Size(677, 436);
+            grpProveedor.TabIndex = 1;
+            grpProveedor.TabStop = false;
+            grpProveedor.Text = "Proveedor";
+            grpProveedor.Enter += grpProveedor_Enter;
             // 
-            // label1
+            // lblEditar
             // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Location = new Point(37, 45);
-            label1.Name = "label1";
-            label1.Size = new Size(176, 21);
-            label1.TabIndex = 0;
-            label1.Text = "Nombre del proveedor";
-            label1.Click += label1_Click;
+            lblEditar.AutoSize = true;
+            lblEditar.Location = new Point(208, 24);
+            lblEditar.Name = "lblEditar";
+            lblEditar.Size = new Size(157, 21);
+            lblEditar.TabIndex = 14;
+            lblEditar.Text = "Editando Proveedor";
+            lblEditar.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(247, 389);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 40);
+            button1.TabIndex = 12;
+            button1.Text = "Confirmar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Activo", "Inactivo" });
+            comboBox1.Location = new Point(37, 328);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(154, 29);
+            comboBox1.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(37, 235);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(154, 26);
+            textBox5.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(37, 152);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(154, 26);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(37, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(154, 26);
+            textBox1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Location = new Point(33, 304);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 21);
+            label6.TabIndex = 5;
+            label6.Text = "Estado";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Location = new Point(37, 211);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 21);
+            label5.TabIndex = 4;
+            label5.Text = "Teléfono";
             // 
             // label4
             // 
@@ -96,88 +178,72 @@
             label4.TabIndex = 3;
             label4.Text = "Correo Electrónico";
             // 
-            // label5
+            // label1
             // 
-            label5.AutoSize = true;
-            label5.BackColor = Color.Transparent;
-            label5.Location = new Point(37, 211);
-            label5.Name = "label5";
-            label5.Size = new Size(76, 21);
-            label5.TabIndex = 4;
-            label5.Text = "Teléfono";
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(37, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre del proveedor";
+            label1.Click += label1_Click;
             // 
-            // label6
+            // dgvProveedores
             // 
-            label6.AutoSize = true;
-            label6.BackColor = Color.Transparent;
-            label6.Location = new Point(33, 304);
-            label6.Name = "label6";
-            label6.Size = new Size(61, 21);
-            label6.TabIndex = 5;
-            label6.Text = "Estado";
+            dgvProveedores.Anchor = AnchorStyles.Right;
+            dgvProveedores.BackgroundColor = Color.White;
+            dgvProveedores.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProveedores.Location = new Point(364, 7);
+            dgvProveedores.Name = "dgvProveedores";
+            dgvProveedores.Size = new Size(536, 158);
+            dgvProveedores.TabIndex = 3;
+            dgvProveedores.CellContentClick += dgvProveedores_CellContentClick;
             // 
-            // textBox1
+            // panel1
             // 
-            textBox1.Location = new Point(37, 69);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(154, 26);
-            textBox1.TabIndex = 6;
+            panel1.Anchor = AnchorStyles.None;
+            panel1.Controls.Add(cbxEliminar);
+            panel1.Controls.Add(cbxEditar);
+            panel1.Controls.Add(cbxAñadir);
+            panel1.Location = new Point(939, 14);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(99, 144);
+            panel1.TabIndex = 15;
+            panel1.Paint += panel1_Paint;
             // 
-            // textBox3
+            // cbxEliminar
             // 
-            textBox3.Location = new Point(37, 152);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(154, 26);
-            textBox3.TabIndex = 8;
+            cbxEliminar.AutoSize = true;
+            cbxEliminar.Location = new Point(12, 90);
+            cbxEliminar.Name = "cbxEliminar";
+            cbxEliminar.Size = new Size(69, 19);
+            cbxEliminar.TabIndex = 14;
+            cbxEliminar.Text = "Eliminar";
+            cbxEliminar.UseVisualStyleBackColor = true;
+            cbxEliminar.CheckedChanged += cbxEliminar_CheckedChanged;
             // 
-            // textBox5
+            // cbxEditar
             // 
-            textBox5.Location = new Point(37, 235);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(154, 26);
-            textBox5.TabIndex = 10;
+            cbxEditar.AutoSize = true;
+            cbxEditar.Location = new Point(12, 53);
+            cbxEditar.Name = "cbxEditar";
+            cbxEditar.Size = new Size(56, 19);
+            cbxEditar.TabIndex = 13;
+            cbxEditar.Text = "Editar";
+            cbxEditar.UseVisualStyleBackColor = true;
+            cbxEditar.CheckedChanged += cbxEditar_CheckedChanged;
             // 
-            // comboBox1
+            // cbxAñadir
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Activo", "Inactivo" });
-            comboBox1.Location = new Point(37, 328);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(154, 29);
-            comboBox1.TabIndex = 11;
-            // 
-            // button1
-            // 
-            button1.BackColor = SystemColors.ControlDarkDark;
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(247, 389);
-            button1.Name = "button1";
-            button1.Size = new Size(220, 40);
-            button1.TabIndex = 12;
-            button1.Text = "Guardar Proveedor";
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // grpProveedor
-            // 
-            grpProveedor.Anchor = AnchorStyles.None;
-            grpProveedor.BackgroundImage = Properties.Resources.fondo;
-            grpProveedor.Controls.Add(button1);
-            grpProveedor.Controls.Add(comboBox1);
-            grpProveedor.Controls.Add(textBox5);
-            grpProveedor.Controls.Add(textBox3);
-            grpProveedor.Controls.Add(textBox1);
-            grpProveedor.Controls.Add(label6);
-            grpProveedor.Controls.Add(label5);
-            grpProveedor.Controls.Add(label4);
-            grpProveedor.Controls.Add(label1);
-            grpProveedor.Font = new Font("Franklin Gothic Heavy", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            grpProveedor.ForeColor = Color.White;
-            grpProveedor.Location = new Point(203, 176);
-            grpProveedor.Name = "grpProveedor";
-            grpProveedor.Size = new Size(659, 436);
-            grpProveedor.TabIndex = 1;
-            grpProveedor.TabStop = false;
-            grpProveedor.Text = "Añadir Proveedor";
+            cbxAñadir.AutoSize = true;
+            cbxAñadir.Location = new Point(12, 19);
+            cbxAñadir.Name = "cbxAñadir";
+            cbxAñadir.Size = new Size(61, 19);
+            cbxAñadir.TabIndex = 3;
+            cbxAñadir.Text = "Añadir";
+            cbxAñadir.UseVisualStyleBackColor = true;
+            cbxAñadir.CheckedChanged += cbxAñadir_CheckedChanged;
             // 
             // frmProovedoresAdmin
             // 
@@ -188,9 +254,11 @@
             Name = "frmProovedoresAdmin";
             Text = "frmProovedoresAdmin";
             tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)dgvProveedores).EndInit();
             grpProveedor.ResumeLayout(false);
             grpProveedor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProveedores).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -207,5 +275,10 @@
         private Label label4;
         private Label label1;
         private DataGridView dgvProveedores;
+        private CheckBox cbxEliminar;
+        private CheckBox cbxEditar;
+        private CheckBox cbxAñadir;
+        private Panel panel1;
+        private Label lblEditar;
     }
 }
--- Proyecto.InnovaStock/Vistas/frmProovedoresAdmin.cs
+++ Proyecto.InnovaStock/Vistas/frmProovedoresAdmin.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Proyecto.InnovaStock
@@ -15,26 +9,128 @@ namespace Proyecto.InnovaStock
         public frmProovedoresAdmin()
         {
             InitializeComponent();
+
+            grpProveedor.Visible = false;
+            lblEditar.Visible = false;
+            dgvProveedores.Visible = true;
+
+            // Crear columna si no existe NINGUNA
+            if (dgvProveedores.ColumnCount == 0)
+            {
+                DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn();
+                col.Name = "MensajeSistema";
+                col.HeaderText = "";
+                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dgvProveedores.Columns.Add(col);
+            }
+
+            // Eventos
+            cbxAñadir.CheckedChanged += CheckBoxes_CheckedChanged;
+            cbxEditar.CheckedChanged += CheckBoxes_CheckedChanged;
+            cbxEliminar.CheckedChanged += CheckBoxes_CheckedChanged;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        // -----------------------------------------------------
+        //                 CONTROL DE CHECKBOXES
+        // -----------------------------------------------------
+        private void CheckBoxes_CheckedChanged(object sender, EventArgs e)
         {
+            if (sender == cbxAñadir && cbxAñadir.Checked)
+            {
+                cbxEditar.Checked = false;
+                cbxEliminar.Checked = false;
+            }
+            else if (sender == cbxEditar && cbxEditar.Checked)
+            {
+                cbxAñadir.Checked = false;
+                cbxEliminar.Checked = false;
+            }
+            else if (sender == cbxEliminar && cbxEliminar.Checked)
+            {
+                cbxAñadir.Checked = false;
+                cbxEditar.Checked = false;
+            }
 
+            ActualizarVisibilidadElementos();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        // -----------------------------------------------------
+        //           MENSAJE ROJO "ELIMINAR"
+        // -----------------------------------------------------
+        private void MostrarMensajeEliminar()
         {
+            // Si ya está el mensaje → NO duplicar
+            if (dgvProveedores.Rows.Count > 0 &&
+                dgvProveedores.Rows[0].Tag?.ToString() == "AlertaEliminar")
+                return;
 
+            DataGridViewRow alerta = new DataGridViewRow();
+            alerta.CreateCells(dgvProveedores);
+
+            alerta.Cells[0].Value = "⚠ Doble Click Para Eliminar Proveedor";
+            alerta.DefaultCellStyle.ForeColor = Color.Red;
+            alerta.DefaultCellStyle.Font = new Font(dgvProveedores.Font, FontStyle.Bold);
+
+            alerta.Tag = "AlertaEliminar";
+
+            dgvProveedores.Rows.Insert(0, alerta);
         }
 
-        private void grpAñadirProve_Enter(object sender, EventArgs e)
+        private void OcultarMensajeEliminar()
         {
-
+            // Eliminar SOLO si es el mensaje
+            if (dgvProveedores.Rows.Count > 0 &&
+                dgvProveedores.Rows[0].Tag?.ToString() == "AlertaEliminar")
+            {
+                dgvProveedores.Rows.RemoveAt(0);
+            }
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        // -----------------------------------------------------
+        //       VISIBILIDAD SEGÚN OPCIÓN SELECCIONADA
+        // -----------------------------------------------------
+        private void ActualizarVisibilidadElementos()
         {
-
+            if (cbxAñadir.Checked)
+            {
+                grpProveedor.Visible = true;
+                lblEditar.Visible = false;
+                OcultarMensajeEliminar();
+            }
+            else if (cbxEditar.Checked)
+            {
+                grpProveedor.Visible = true;
+                lblEditar.Visible = true;
+                OcultarMensajeEliminar();
+            }
+            else if (cbxEliminar.Checked)
+            {
+                grpProveedor.Visible = false;
+                lblEditar.Visible = false;
+                MostrarMensajeEliminar();
+            }
+            else
+            {
+                grpProveedor.Visible = false;
+                lblEditar.Visible = false;
+                OcultarMensajeEliminar();
+            }
         }
+
+        // -----------------------------------------------------
+        // EVENTOS AUTOMÁTICOS DEL FORM (NO USAR)
+        // -----------------------------------------------------
+        private void dgvProveedores_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void grpProveedor_Enter(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void grpAñadirProve_Enter(object sender, EventArgs e) { }
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e) { }
+        private void panel1_Paint(object sender, PaintEventArgs e) { }
+        private void cbxEliminar_CheckedChanged(object sender, EventArgs e) { }
+        private void cbxEditar_CheckedChanged(object sender, EventArgs e) { }
+        private void cbxAñadir_CheckedChanged(object sender, EventArgs e) { }
+        private void label2_Click_1(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
     }
 }
